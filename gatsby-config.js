4c6c8c5e5ffeab0e31e9402ef43303a7a11becc4
Thammarith <8011543+thammarith@users.noreply.github.com>
2022-02-16 13:39:24 +0700

    (feat) Update editor and Gatsby configuration (#4)
    * update editor configuration
    
    * update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,53 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-CTSYFBBWFF"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+	siteMetadata: {
+		siteUrl: 'https://www.yourdomain.tld',
+	},
+	plugins: [
+		// plug-ins
+		'gatsby-plugin-image',
+		'gatsby-plugin-mdx',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sharp',
+		'gatsby-plugin-sitemap',
+		{
+			resolve: 'gatsby-plugin-google-analytics',
+			options: {
+				trackingId: 'G-CTSYFBBWFF',
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-sass',
+			options: {
+				postCssPlugins: [
+					require('tailwindcss'),
+					// require('./tailwind.config.js'), // Optional: Load custom Tailwind CSS configuration
+				],
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: 'src/images/icon.png',
+			},
+		},
+		// transformers
+		'gatsby-transformer-sharp',
+		// sources
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/images/',
+			},
+			__key: 'images',
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: './src/pages/',
+			},
+			__key: 'pages',
+		},
+	],
 };
