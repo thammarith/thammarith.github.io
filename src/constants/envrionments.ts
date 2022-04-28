056c4aa353c7e966658e8ba8f239621fe3f4d743
Thammarith <8011543+thammarith@users.noreply.github.com>
2022-04-28 13:52:17 +0700

    Temp

--- src/constants/envrionments.ts
+++ src/constants/envrionments.ts
@@ -1,2 +1,3 @@
 export const DIRECTUS_URL =	import.meta.env.PUBLIC_DIRECTUS;
+// export const SITE_URL = 'thammarith.github.io';
 export const SITE_URL = import.meta.env.PUBLIC_BASE_URL ?? 'https://thammarith.dev';
