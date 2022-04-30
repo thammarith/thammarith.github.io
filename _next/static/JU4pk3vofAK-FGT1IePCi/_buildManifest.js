ddecb9964219b785ce0a010da9adf57d1bad308c
Thammarith <8011543+thammarith@users.noreply.github.com>
2022-04-30 14:24:13 +0700

    Updates

--- _next/static/JU4pk3vofAK-FGT1IePCi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7e1dea14d2c10484.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],"/blog/[slug]":["static/chunks/pages/blog/[slug]-a1632989fd98ef97.js"],sortedPages:["/","/_app","/_error","/blog/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
