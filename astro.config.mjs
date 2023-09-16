9408b821e5b55dca2b9f2f730bfb6c957e004fac
Thammarith <8011543+thammarith@users.noreply.github.com>
2023-09-16 17:46:43 +0700

    Feat/astro 3.0 (#7)
    * clean up
    
    * install new astro
    
    * Fix formatting
    
    * install Solid
    
    * Configure Tailwind
    
    * Update files
    
    * Fix SEO issue
    
    * Add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 import { defineConfig } from 'astro/config';
+import solidJs from '@astrojs/solid-js';
 
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import astroImagePlugin from "astro-imagetools/plugin";
-
+// https://astro.build/config
 export default defineConfig({
 	site: 'https://thammarith.dev',
-	integrations: [react(), tailwind()],
-	vite: {
-		plugins: [astroImagePlugin],
-	},
-	server: {
-		tailwindConfig: './tailwind.config.js',
-	},
+	integrations: [solidJs(), tailwind()],
 });
--- src/constants/envrionments.ts
+++
@@ -1,2 +0,0 @@
-export const DIRECTUS_URL =	import.meta.env.PUBLIC_DIRECTUS;
-export const SITE_URL = import.meta.env.PUBLIC_BASE_URL ?? 'https://thammarith.dev';
