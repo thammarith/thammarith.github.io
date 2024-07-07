b7ef378a75d15c09a715258c88621cb475fbc71d
Thammarith <8011543+thammarith@users.noreply.github.com>
2024-07-07 18:43:38 +0700

    Update logo logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
 export default () => (
-	<header class="py-4 pt-12 flex justify-center">
-		<div class="max-w-[96ch] w-full lg:rounded-lg">
-			<a
-				href="/"
-				class="inline-block px-4 py-2 -mx-2 -my-2 bg-slate-900 hover:bg-orange-500 rounded transition"
-			>
-				<h2 class="text-slate-100 font-title font-semibold text-3xl">thammarith.dev</h2>
+	<header class="px-4 sm:px-16 pt-12 flex flex-col flex-grow-0 items-center justify-center">
+		<div class="w-full max-w-[72ch]">
+			<a href="/" class="inline-block px-4 py-2 bg-slate-900 hover:bg-orange-500 rounded transition">
+				<h2 title="Homepage" class="text-slate-100 font-title font-semibold text-3xl">
+					thammarith
+				</h2>
 			</a>
 		</div>
 	</header>
