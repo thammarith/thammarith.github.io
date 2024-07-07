8f6b26fa5c0dd6c538da83eeb583046006bad6df
Thammarith <8011543+thammarith@users.noreply.github.com>
2024-07-07 18:16:47 +0700

    try this

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default () => (
+	<header class="py-4 pt-12 flex justify-center">
+		<div class="max-w-[96ch] w-full lg:rounded-lg">
+			<a
+				href="/"
+				class="inline-block px-4 py-2 -mx-2 -my-2 bg-slate-900 hover:bg-orange-500 rounded transition"
+			>
+				<h2 class="text-slate-100 font-title font-semibold text-3xl">thammarith.dev</h2>
+			</a>
+		</div>
+	</header>
+);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const baseSansSerif = ['ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'];
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				title: ['Poppins', ...baseSansSerif],
+				heading: ['Figtree', 'Anuphan', ...baseSansSerif],
+			},
+		},
 	},
 	plugins: [],
 };
