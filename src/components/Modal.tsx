8af532e066e6bcf59b706b639b43ae9f4f3c8391
Thammarith <8011543+thammarith@users.noreply.github.com>
2024-08-11 18:50:02 +0700

    Update experience card to show more details

---
+++ src/components/Modal.tsx
@@ -0,0 +1,47 @@
+import { onCleanup, onMount, type FlowComponent } from 'solid-js';
+import { Portal } from 'solid-js/web';
+
+export type ModalProps = {
+	onClose: () => any;
+	classList?: string;
+};
+
+export const Modal: FlowComponent<ModalProps> = (props) => {
+	let ref: HTMLDialogElement = undefined as unknown as HTMLDialogElement;
+
+	const onClose = () => {
+		document.querySelector('body')?.classList.remove('modal--open');
+		props.onClose();
+	};
+
+	onMount(() => {
+		document.querySelector('body')?.classList.add('modal--open');
+		ref?.showModal();
+		ref?.addEventListener('close', onClose);
+	});
+
+	onCleanup(() => {
+		ref?.removeEventListener('close', onClose);
+	});
+
+	return (
+		<Portal>
+			<dialog
+				class="fixed p-12 backdrop:backdrop-blur-sm rounded-md"
+				classList={{
+					[props.classList]: !!props.classList,
+				}}
+				ref={ref}
+			>
+				<button
+					class="absolute top-4 right-4 opacity-50 hover:opacity-100 hover:text-red-500"
+					title="Close"
+					onClick={onClose}
+				>
+					<i class="fa-regular fa-xmark"></i>
+				</button>
+				{props.children}
+			</dialog>
+		</Portal>
+	);
+};
--- src/components/index/ExperienceCard.tsx
+++ src/components/index/ExperienceCard.tsx
@@ -1,15 +1,17 @@
-import type { Component } from 'solid-js';
+import { createSignal, type Component } from 'solid-js';
 
-import AgodaLogo from '@assets/logos/agoda.svg'
-import ViabusDark from '@assets/logos/viabus-wordmark-dark.svg'
-import TedXMahidolU from '@assets/logos/tedx-mahidolu.svg'
+import AgodaLogo from '@assets/logos/agoda.svg';
+import ViabusDark from '@assets/logos/viabus-wordmark-dark.svg';
+import TedXMahidolU from '@assets/logos/tedx-mahidolu.svg';
 import Empty from '@assets/logos/_empty.svg';
+
+import { Modal, type ModalProps } from '@components/Modal';
 
 type Position = {
 	title: string;
 	startDate: Date;
 	endDate?: Date;
-}
+};
 
 type Props = {
 	positions: Position[];
@@ -24,24 +26,58 @@ const getLogo = (slug: Props['slug']): ImageMetadata => {
 	if (is('viabus')) return ViabusDark;
 	if (is('tedxmahidolu')) return TedXMahidolU;
 	return Empty;
-}
+};
+
+const formatServiceYear = (date?: Date) => date?.toLocaleDateString('en-gb', { dateStyle: 'long' }) ?? 'present';
 
 const getServiceYear = (positions: Props['positions']) => {
-	const [start, end] = [positions.at(-1)?.startDate?.getFullYear(), positions.at(0)?.endDate?.getFullYear()]
+	const [start, end] = [positions.at(-1)?.startDate?.getFullYear(), positions.at(0)?.endDate?.getFullYear()];
 	if (start === end) return `${start}`;
-	return`${start} – ${end ?? 'present'}`;
-}
+	return `${start} – ${end ?? 'present'}`;
+};
 
-export const ExperienceCard: Component<Props> = (props) => (
-	<div class="flex-auto md:flex-1 shadow-lg p-8 rounded">
-		<div class="flex gap-4 md:block">
-			<div class="w-20 h-12 flex items-center justify-start">
-				<img src={getLogo(props.slug).src} alt={props.slug} class="max-h-8" />
-			</div>
-			<div>
-				<h4 class="font-semibold text-lg">{getPeakPosition(props.positions)}</h4>
-				<p class="text-sm">{getServiceYear(props.positions)}</p>
-			</div>
+export const ExperienceDetail: Component<ModalProps & Props> = (props) => (
+	<Modal onClose={props.onClose} classList="min-w-[48dvw]">
+		<img src={getLogo(props.slug).src} alt={props.slug} class="max-h-8" />
+		<div class="mt-4">
+			{props.positions
+				.sort((a, b) => b.startDate.valueOf() - a.startDate.valueOf())
+				.map((position) => (
+					<>
+						<h2 class="mt-2 font-semibold">{position.title}</h2>
+						<p>
+							({formatServiceYear(position.startDate)}&ndash;{formatServiceYear(position.endDate)})
+						</p>
+					</>
+				))}
 		</div>
-	</div>
+	</Modal>
 );
+
+export const ExperienceCard: Component<Props> = (props) => {
+	const [isShowingDialogue, setIsShowingDialogue] = createSignal(false);
+
+	const openDialogue = () => {
+		setIsShowingDialogue(() => true);
+	};
+	const closeDialogue = () => {
+		setIsShowingDialogue(() => false);
+	};
+
+	return (
+		<>
+			{isShowingDialogue() && <ExperienceDetail onClose={closeDialogue} {...props} />}
+			<div class="flex-auto md:flex-1 shadow-md p-8 rounded hover:cursor-pointer" onClick={openDialogue}>
+				<div class="flex gap-4 md:block">
+					<div class="w-20 h-12 flex items-center justify-start">
+						<img src={getLogo(props.slug).src} alt={props.slug} class="max-h-8" />
+					</div>
+					<div>
+						<h4 class="font-semibold text-lg">{getPeakPosition(props.positions)}</h4>
+						<p class="text-sm">{getServiceYear(props.positions)}</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
