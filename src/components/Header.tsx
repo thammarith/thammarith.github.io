0663bca838b13c661f3d336617a4a811eb6690e9
Thammarith Likittheerameth <thammarith.likittheerameth@agoda.com>
2025-01-17 10:49:35 +0700

    Rearrange styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
 export default () => (
 	<header class="px-4 sm:px-16 pt-12 flex flex-col flex-grow-0 items-center justify-center">
 		<div class="w-full max-w-[72ch]">
-			<a href="/" class="inline-block px-4 py-2 rounded bg-space-orange hover:bg-transparent transition">
-				<h2 title="Homepage" class="text-white hover:text-space-orange font-title font-semibold text-3xl transition">
+			<a
+				href="/"
+				class="inline-block px-4 py-2 rounded bg-space-orange hover:bg-transparent text-white hover:text-space-orange transition"
+			>
+				<h2 title="Homepage" class="font-title font-semibold text-3xl">
 					thammarith
 				</h2>
 			</a>
